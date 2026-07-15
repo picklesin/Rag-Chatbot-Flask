d8a63efb0ca6c7fdf716eb995922967c6cbcc55e
Joseph Yim <josephyim@Josephs-MacBook-Air-3.local>
2026-07-15 18:33:15 -0400

    updated files

--- app/rag_agent.py
+++ app/rag_agent.py
@@ -1,27 +1,14 @@
 import os
 import uuid
+from flask import session
 from langchain.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_chroma import Chroma
-from flask import session
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-
-# Create Emdeddings Model
-def load_vector_store():
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",
-                                                api_key=os.environ["GOOGLE_API_KEY"],)
-    store = Chroma(
-        collection_name="pdf-collection",
-        embedding_function=embeddings,
-        persist_directory='chroma',
-    )
-
-    return store
 
 
 def text_splitter(file_path):
@@ -45,10 +32,25 @@ def ingest_pdf(file_path):
     vector_store.add_documents(docs)
 
 
+# Create Emdeddings Model
+def load_vector_store():
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",
+                                                api_key=os.environ["GOOGLE_API_KEY"],)
+    store = Chroma(
+        collection_name="pdf-collection",
+        embedding_function=embeddings,
+        persist_directory='chroma',
+    )
+
+    return store
+
+vector_store = load_vector_store()
+
+
+
 # Build rag agent using Gemini
-def build_rag_agent():
-    vector_store = load_vector_store()
-  
+def build_rag_agent(vector_store):
+     
     @tool(response_format="content_and_artifact")
     def retrieve_content(query: str):
         """Retrieve information to help answer a query"""
@@ -58,7 +60,6 @@ def build_rag_agent():
         )
 
         return serialized, retrieved_docs
-
 
     model = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
@@ -86,20 +87,40 @@ def build_rag_agent():
 
     return agent
 
+
+agent = build_rag_agent(vector_store)
+
+
 # Gemini rag response
 def chat_response(question):
-    agent = build_rag_agent()
+
+    if "thread_id" not in session:
+        session["thread_id"] = str(uuid.uuid4())
+
+    thread_id = session["thread_id"]
 
     result = agent.invoke(
     {"messages": [{"role": "user", "content": question}]},
+    {"configurable": {"thread_id": thread_id}},
     )
 
     response = result["messages"][-1].content
 
-
     if isinstance(response, list):
         final_response = response[0]["text"]
+    else:
+        final_response = response
+
                         
     return final_response
     
 
+# added agent and vector_store at global level
+
+
+'''
+
+Error calling model 'gemini-2.5-flash' (NOT_FOUND): 404 NOT_FOUND. {'error': {'code': 404, 'message': 'This model models/gemini-2.5-flash is no longer available to new users. 
+Please update your code to use a newer model for the latest features and improvements.', 'status': 'NOT_FOUND'}}
+
+'''
