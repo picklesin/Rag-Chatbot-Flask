84f70cb8b869f3378636aed994e29659b4b935dd
Joseph Yim <josephyim@Josephs-MacBook-Air-3.local>
2026-07-18 12:55:20 -0400

    changed env names and config names

--- app/config.py
+++ app/config.py
@@ -3,10 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
-
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+ 
+    GEMINI_API_KEY = os.getenv('GOOGLE_API_KEY')
 
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
